445a509e00062e945d0a0f22ce8009125f2fa3d2
Athar El-Haidary <48630732+asar17@users.noreply.github.com>
2023-03-02 02:22:24 +0200

    SWR data fetching

---
+++ src/pages/dashboard-swr.js
@@ -0,0 +1,22 @@
+import useSWR from 'swr'
+
+const DashboardSwr =()=>{
+    const fetcher =async()=>{
+        const res=await fetch('http://localhost:4000/dashboard')
+        const data=await res.json()
+        return data
+    
+    }
+   
+    const {data}=useSWR('dashboard',fetcher)
+    return(
+        <div>
+            <h2>Dashboard</h2>
+            <h2>{data?.posts}</h2>
+            <h2>{data?.likes}</h2>
+            <h2>{data?.followers}</h2>
+            <h2>{data?.following}</h2>
+        </div>
+    )
+}
+export default DashboardSwr
